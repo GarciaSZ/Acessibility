0b6fa918fbe6436dde3743ed6847fe3cfca95bb4
Arthur Garcia <arthursouzagarcia21@gmail.com>
2026-09-09 16:03:55 -0300

    Upd 1

--- Acessiblidade/js/script.js
+++ Acessiblidade/js/script.js
@@ -1,6 +1,32 @@
 let tamanhoGrande = false;
 
 function aumentarTexto() {
+    document.body.classList.remove("texto-pequeno");
     document.body.classList.add("texto-grande");
     tamanhoGrande = true;
+}
+
+let tamanhoPequeno = false;
+
+function diminuirTexto() {
+    document.body.classList.remove("texto-grande");
+    document.body.classList.add("texto-pequeno");
+    tamanhoPequeno = true;
+}
+
+function altoContraste() {
+    document.body.classList.toggle("contraste");
+}
+
+function lerPagina() {
+    speechSynthesis.cancel();
+    const texto =document.getElementById("conteudo").innerText;
+    const leitura = new SpeechSynthesisUtterance(texto);
+    leitura.lang = "pt-BR";
+    leitura.rate = 1.5;
+    speechSynthesis.speak(leitura);
+}
+
+function pararLeitura() {
+    speechSynthesis.cancel();
 }
